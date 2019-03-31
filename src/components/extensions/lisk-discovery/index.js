9482c70566ead198e63cf5eb207d911f4717af86
Tom Ploem <tom.ploem@gmail.com>
2019-03-31 21:20:10 +0200

    LDH-01 hello world template extension

---
+++ src/components/extensions/lisk-discovery/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import LiskHubExtensions from 'liskHubExtensions';
+//import LiskHubExtensions from '../../../utils/liskHubExtensions';
+
+const Box = LiskHubExtensions.components.Box;
+
+class LiskDiscovery extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      projects: [],
+    };
+  }
+
+  render() {
+    return (
+      <Box>
+        <h1>hello world</h1>
+      </Box>
+    );
+  }
+}
+
+export default LiskDiscovery;
+
+LiskHubExtensions.addModule({
+  identifier:
+  LiskHubExtensions.identifiers.dashboardColumn1,
+  component: LiskDiscovery,
+});
